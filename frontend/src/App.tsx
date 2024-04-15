239cb90d489bebb4bf61be99ef4b8201081ac8a4
Quintexxx <martin.brunner06@gmail.com>
2024-04-15 16:13:45 +0200

    Initial Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import { Badge, Button } from '@mui/joy';
+import { styled } from '@mui/joy';
+import { Route, Routes } from 'react-router-dom';
+import { DetailPage } from './pages/DetailPage.tsx';
+import { HomePage } from './pages/HomePage.tsx';
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-      <Button>Button</Button>
-      <Badge style={{ backgroundColor: 'green' }} />
-      <Button disabled>Disabled</Button>
-      <Button loading>Loading</Button>
-    </>
+    <Wrapper>
+      <Routes>
+        <Route path={'/'} element={<HomePage />} />
+        <Route path={'/detail'} element={<DetailPage />} />
+      </Routes>
+    </Wrapper>
   );
 }
 
+const Wrapper = styled('div')`
+  min-height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr;
+`;
 export default App;
